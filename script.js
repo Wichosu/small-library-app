f1018f3585032cd284acd0f68654373558db2551
Wicho <luiscalderonmiranda@gmail.com>
2022-07-01 21:22:47 -0500

    added new book button and form template

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const addButton = document.getElementById('addButton')
+const newBookButton = document.getElementById('newBookButton')
+const newBookForm = document.getElementById('newBookForm')
+
+const summonForm = () => {
+  newBookForm.style.visibility = 'visible'
+  newBookButton.style.visibility = 'hidden'
+}
+
+newBookButton.addEventListener('click', summonForm, false)
+
